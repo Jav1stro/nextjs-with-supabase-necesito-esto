b2901d17505c487569e17ea5dfc3d74122fb54f7
Agustin-Monti <agustinnicolasmonti@gmail.com>
2024-09-30 12:42:26 -0300

    header_cambios2
    hola ahi agregue 10 cambios despues ustedes si quieren lo modifican o lo cambian no hay problema es para ir probando.  1- agregue el getDemandas para traer las demandas de la db, 2- agregue el metodo de getDemandas en client.ts, 3- agregue el header que habia echo en el layout implementando el search de alan, 4- agregue a componentes demandasCliente donde muestro las demandas, 5- agregue en componentes ModalDetallePago para mostrar el modal para implementar alguna funcion de pago y un link directo al detalle, 6- cree la carpeta demandas para la pagina demandas, 7- agregue la carpeta details-demanda/[id] para mostrar los detalles segun el id de la demanda. y los otros son cambios de eliminar algunas cosas boludas y agregar algunos estilos nada mas.

---
+++ components/ModalDetallesPago.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from "next/link";
+
+
+interface ModalDetallesPagoProps {
+  isOpen: boolean;
+  onClose: () => void;
+  demanda: any;  // Cambia 'any' por el tipo correcto de demanda si lo tienes
+}
+
+const ModalDetallesPago: React.FC<ModalDetallesPagoProps> = ({ isOpen, onClose, demanda }) => {
+  if (!isOpen) return null;  // Si el modal no está abierto, no mostrar nada
+
+  return (
+    <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full negro">
+        {/* Contenido del modal */}
+        <h2 className="text-xl font-bold mb-4">Detalles de la Demanda</h2>
+
+        <h3 className="font-bold text-lg ito">{demanda.detalle}</h3>
+        <p className="ito"><strong>Rubro:</strong> {demanda.rubro_demanda}</p>
+        <p className="ito"><strong>Fecha de inicio:</strong> {new Date(demanda.fecha_inicio).toLocaleDateString()}</p>
+        <p className="ito"><strong>Fecha de vencimiento:</strong> {new Date(demanda.fecha_vencimiento).toLocaleDateString()}</p>
+        {/* Puedes agregar más información aquí */}
+        
+        {/* Botón para proceder con el pago */}
+        <div className="mt-4">
+          <Link className="bg-green-500 text-white py-2 px-4 rounded-lg" href={`/details-demanda/${demanda.id}`}>
+            Pagar para obtener más detalles
+          </Link>
+        </div>
+        
+        {/* Botón para cerrar el modal */}
+        
+        <button onClick={onClose} className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
+          Cerrar 
+        </button>
+      
+        
+      </div>
+    </div>
+  );
+};
+
+export default ModalDetallesPago;
