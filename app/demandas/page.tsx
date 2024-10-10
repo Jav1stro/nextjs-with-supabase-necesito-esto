d479f58e8cea36f9c66ff5db12ff4a81ee7aac61
Alan-H-H <123487644+Alan-H-H@users.noreply.github.com>
2024-10-10 20:49:53 -0300

    Filtro funcional
    Carga en suspenso del lado cliente.
    
    Filter demandas by select.

--- app/demandas/page.tsx
+++ app/demandas/page.tsx
@@ -1,8 +1,9 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import DemandasCliente from "@/components/DemandasCliente";
 import { getAllDemandas } from "@/actions/demanda-actions";
+import { Suspense } from "react";
+import MyLoader from "@/components/SkeletonLoader";
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -21,21 +22,10 @@ export default async function ProtectedPage() {
     <main className="flex-1 flex flex-col gap-6 px-4 demandas">
       <h2 className="font-medium text-xl mb-4">Demandas</h2>
 
-      <div className="mb-4 flex items-center">
-        <AdjustmentsHorizontalIcon className="h-5 w-5 text-gray-500 mr-2" />{" "}
-        <label htmlFor="categoria" className="mr-2">
-          Filtrar por categoría:
-        </label>
-        <select>
-          <option>Todas</option>
-          <option>Sistemas</option>
-          <option>Mantenimiento</option>
-          <option>Seguridad</option>
-          <option>Marketing</option>
-        </select>
-      </div>
-
+      
+    <Suspense fallback={<MyLoader />}>
       <DemandasCliente demandas={demandas} />
+    </Suspense>
     </main>
   );
 }
